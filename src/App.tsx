b8a3e99a3c862cb862da8bf22b8a2b207b99c6e4
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2025-01-02 12:25:01 -0500

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" max-w-[400px] bg-red-500 mx-auto mt-28 h-20 items-center justify-center flex rounded-lg shadow-2xl">
+      <h1 className=" text-white font-semibold text-center text-[50px]">
+        Hola mundo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
